19223e84773c585b255f11693fd4ea6eb199898e
mura9916 <natsu8199916@gmail.com>
2023-10-23 14:56:13 +0900

    パッチの特徴量の中央値を取得するように変更

--- texture_extractor.py
+++ texture_extractor.py
@@ -8,7 +8,9 @@ import matplotlib.pyplot as plt
 import six
 
 class TextureExtractor:
-    def __init__(self) -> None:
+    def __init__(self, clip_size=64, num_sample=100) -> None:
+        self.clip_size = clip_size
+        self.num_sample = num_sample
         self.feature_class_func = {
             "FO" : firstorder.RadiomicsFirstOrder,
             "GLCM" : glcm.RadiomicsGLCM,
@@ -22,11 +24,16 @@ class TextureExtractor:
         image = sitk.GetImageFromArray( np.expand_dims(image_array, axis=0) )
         mask = sitk.GetImageFromArray( np.expand_dims(mask_array, axis=0))
         return image, mask
-        
+
+    def get_clip_image(self, image, clip_size):
+        size = image.shape[0]
+        center_x = int( np.random.uniform(clip_size//2, size-clip_size//2) )
+        center_y = int( np.random.uniform(clip_size//2, size-clip_size//2) )
+        clip_image = image[center_x-clip_size//2:center_x+clip_size//2, center_y-clip_size//2:center_y+clip_size//2]
+        return clip_image
+
     def get_texture_feature(self, image_array, mask_array, settings={}):
-        
         image, mask = self.parse_input_data(image_array, mask_array)
-        
         result = {}
         for class_key in self.feature_class_func.keys():
             feature_extractor = self.feature_class_func[class_key](image, mask, **settings)
@@ -35,6 +42,19 @@ class TextureExtractor:
                 result[f'{class_key}-{feature_key}'] = np.float64(value)
         
         return result
+    
+    def get_median_texture_feature_from_patch(self, image):
+        if( min(image.shape) > self.clip_size ):
+            print('ERROR')
+            return 0
+        
+        mask = np.ones((self.clip_size, self.clip_size))
+        feature_df = pd.DataFrame()
+        for i in range(self.num_sample):
+            clip_image = self.get_clip_image(image, self.clip_size)
+            result = self.get_texture_feature(clip_image, mask)
+            feature_df = pd.concat([feature_df, pd.DataFrame(result, index=[0])], ignore_index=True)
+        return feature_df.median().to_dict()
 
 if __name__ == "__main__":
     print('say, hello')
