b504a020bfdfb24a7fd537a84e7ed9e27af1d772
mura9916 <natsu8199916@gmail.com>
2023-10-23 15:10:15 +0900

    グレーレベルの設定を可能に

--- texture_extractor.py
+++ texture_extractor.py
@@ -8,9 +8,10 @@ import matplotlib.pyplot as plt
 import six
 
 class TextureExtractor:
-    def __init__(self, clip_size=64, num_sample=100) -> None:
+    def __init__(self, clip_size=64, num_sample=100, setting={"binWidth": 10}) -> None:
         self.clip_size = clip_size
         self.num_sample = num_sample
+        self.setting = setting
         self.feature_class_func = {
             "FO" : firstorder.RadiomicsFirstOrder,
             "GLCM" : glcm.RadiomicsGLCM,
@@ -32,11 +33,11 @@ class TextureExtractor:
         clip_image = image[center_x-clip_size//2:center_x+clip_size//2, center_y-clip_size//2:center_y+clip_size//2]
         return clip_image
 
-    def get_texture_feature(self, image_array, mask_array, settings={}):
+    def get_texture_feature(self, image_array, mask_array):
         image, mask = self.parse_input_data(image_array, mask_array)
         result = {}
         for class_key in self.feature_class_func.keys():
-            feature_extractor = self.feature_class_func[class_key](image, mask, **settings)
+            feature_extractor = self.feature_class_func[class_key](image, mask, **self.setting)
             feature_extractor.enableAllFeatures()
             for (feature_key, value) in six.iteritems(feature_extractor.execute()):
                 result[f'{class_key}-{feature_key}'] = np.float64(value)
@@ -44,7 +45,8 @@ class TextureExtractor:
         return result
     
     def get_median_texture_feature_from_patch(self, image):
-        if( min(image.shape) > self.clip_size ):
+        
+        if( min(image.shape) < self.clip_size ):
             print('ERROR')
             return 0
         
